b5db0d0992a0a605dd92efca409ef2f5d023deb1
Manoj Nama <manoj.nama@outlook.com>
2014-06-02 12:21:42 +0530

    Allowed config to be read from anywhere inside the app

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var appRouter = require("./config/URLMappings");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 
+var config = require("./config/Config.json");
+
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser());
 
 appRouter.addRoutes(app);
+
+GLOBAL._config = config;
 
 app.use(express.static(__dirname + '/public'));
 
