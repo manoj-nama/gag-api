aeb96cb6ae456d2d1138b6ad28956131b9924138
Manoj Nama <manoj.nama@outlook.com>
2014-06-05 10:03:04 +0530

    Updated gag showing module, sanity commit, untested

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,41 @@ var app = angular.module("GagApp", ["ngRoute"]);
 app.globalScope = {};
 
 function GagController ($scope, $http, $location, $routeParams, $rootScope) {
-	loadGags($http, $routeParams.category, $routeParams.pageId, function(resp) {
-		console.log(resp);
-		if(resp.data) {
-			$rootScope.gags = resp.data;
-			$rootScope.nextPageId = resp.paging.next;
+	var gagId = $routeParams.gagId || 1;
+	function lazyLoadGags() {
+		$rootScope.hasMore && loadGags($http, $routeParams.category, $routeParams.pageId, function(resp) {
+			console.log(resp);
+			if(resp.data) {
+				$scope.gags = resp.data;
+				$scope.nextPageId = resp.paging.next;
+				$scope.totalGags = resp.data.length;
+				$scope.currentGag = $scope.gags[gagId - 1];
+				$rootScope.hasMore = false;
+			}
+		});
+	}
+
+	lazyLoadGags();
+
+	$scope.showGag = function (direction) {	
+		var gagId = (!isNaN($routeParams.gagId)) ? $routeParams.gagId : 1;
+		if(direction) {
+			switch(direction) {
+				case 'next':
+					gagId++;
+					break;
+				case 'prev':
+					gagId--;
+			}
+			console.log(gagId);
+			$scope.currentGag = $scope.gags[gagId - 1];
+			if(gagId > $scope.totalGags - 1) {
+				lazyLoadGags();
+			}
 		}
-	});
+	}
+
+	// $scope.nextGag = ($routeParams.gagId < $rootScope.totalGags) ? 
 }
 
 function loadGags(http, category, pageId, callback) {
@@ -17,18 +45,18 @@ function loadGags(http, category, pageId, callback) {
 } 
 
 function InitController ($location) {
-	$location.path("/gag/hot/0");
+	$location.path("/gag/hot/0/1");
 }
-function MasterController ($scope, $http, $location) {
-	console.log("MasterController");
+function MasterController ($scope, $http, $location, $rootScope) {
+	$rootScope.hasMore = true;
 	$scope.loadGagCategory = function (categoryName) {
-		$location.path("/gag/" + categoryName + "/0");
+		$location.path("/gag/" + categoryName + "/0/1");
 	}
 }
 
 app.config(["$routeProvider", function ($routeProvider) {
 	$routeProvider
 		.when("/", {template: "<div></div>", controller: InitController})
-		.when("/gag/:category/:pageId", {templateUrl: "/views/gag.html", controller: GagController})
+		.when("/gag/:category/:pageId/:gagId", {templateUrl: "/views/gag.html", controller: GagController})
 		.otherwise({redirectTo: "/"});
 }]);
